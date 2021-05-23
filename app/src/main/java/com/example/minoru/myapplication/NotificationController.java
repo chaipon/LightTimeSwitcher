29e4220a663658c8c45ac2a76033b19764e5ce6b
chaipon <mekun@be.to>
2021-05-23 23:55:26 +0900

    Merge AndroidX

--- app/src/main/java/com/example/minoru/myapplication/NotificationController.java
+++ app/src/main/java/com/example/minoru/myapplication/NotificationController.java
@@ -6,10 +6,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
-import android.support.v4.app.NotificationCompat;
-import android.util.Log;
-
-import static android.provider.Settings.System.getString;
+import androidx.core.app.NotificationCompat;
 
 /**
  * Created by Minoru on 2016/11/06.
