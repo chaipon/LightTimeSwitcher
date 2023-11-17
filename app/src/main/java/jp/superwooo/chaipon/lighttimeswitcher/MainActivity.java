5b475cf80c27ee61c892f1025dfd6c189b6cc6f4
Minoru <mekun@be.to>
2023-11-17 22:59:40 +0900

    パッケージ名変更 com.example.minoru.myaplication -> jp.superwooo.chaipon.lighttimeswitcher

---
+++ app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/MainActivity.java
@@ -0,0 +1,130 @@
+package jp.superwooo.chaipon.lighttimeswitcher;
+
+import android.content.ContentResolver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+
+import static android.provider.Settings.*;
+import static android.widget.Toast.*;
+
+import com.example.minoru.myapplication.R;
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class MainActivity extends AppCompatActivity {
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    //private GoogleApiClient client;
+    public static final Integer MinTime = 15 * 1000;
+    public static final Integer MaxTime = 30 * 60 * 1000;
+    Integer mMinTime = MinTime;
+    Integer mMaxTime = MaxTime;
+    private Integer mTimeOut = mMinTime;
+    private StringBuilder mTimeOutMessage = new StringBuilder();
+    public boolean isMinimumTimeOut(){
+        return mTimeOut.equals(mMinTime);
+    }
+    public StringBuilder getTimeoutMessage(){
+
+        return mTimeOutMessage;
+    }
+    ActivityResultLauncher mRequestPermissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted ->{
+        execBody();
+    });
+    ActivityResultLauncher mStartLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), result ->{
+        if(Settings.System.canWrite(getApplicationContext()))
+            mRequestPermissionLauncher.launch("android.permission.POST_NOTIFICATIONS");
+        else
+            showExplainToSetSystemSettings();
+    });
+
+    private Intent makeNotificationSettingIntent(Context context) {
+        Intent intent = new Intent();
+        intent.setAction(Settings.ACTION_APP_NOTIFICATION_SETTINGS);
+        intent.setPackage(context.getPackageName());
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            intent.setIdentifier(String.valueOf(context.getApplicationInfo().uid));
+        }
+        return intent;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        mMinTime = preferences.getInt(SettingsActivity.MinimumKey, MinTime / 1000) * 1000;
+        mMaxTime = preferences.getInt(SettingsActivity.MaximumKey, MaxTime / 1000) * 1000;
+
+        if(Settings.System.canWrite(getApplicationContext()))
+            execBody();
+        else {
+            setTheme(R.style.Base_Theme_AppCompat);
+            setContentView(R.layout.explain_to_setting_system_permissions);
+        }
+
+    }
+
+    private void showExplainToSetSystemSettings() {
+        makeText(getApplicationContext(), "Please set system permission.", LENGTH_SHORT).show();
+    }
+
+    private void execBody() {
+        switchTimeOut();
+        makeTimeOutMessage();
+        showTimeOutMessageToToast();
+        notifyTimeOut();
+        this.finish();
+    }
+
+    private void notifyTimeOut() {
+        NotificationController notification = new NotificationController(this);
+        notification.notifyTimeOut();
+    }
+
+    private void makeTimeOutMessage() {
+        mTimeOutMessage.append(getString(R.string.setting_message, mTimeOut/1000));
+    }
+
+    private void showTimeOutMessageToToast() {
+        makeText(getApplicationContext(), mTimeOutMessage.toString(), LENGTH_SHORT).show();
+    }
+
+    private void switchTimeOut() {
+        ContentResolver cr = getContentResolver();
+        try {
+            mTimeOut = Settings.System.getInt(cr, Settings.System.SCREEN_OFF_TIMEOUT);
+        } catch (SettingNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(mTimeOut.equals(mMinTime)){
+            Log.d("timeOut", "set to max");
+            mTimeOut = mMaxTime;
+        }else{
+            Log.d("timeOut", "set to min ###################### ");
+            mTimeOut = mMinTime;
+        }
+        Settings.System.putInt(cr, Settings.System.SCREEN_OFF_TIMEOUT, mTimeOut);
+    }
+
+    public void goToSystemSettings(View view) {
+        Intent permissionIntent = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS);
+        permissionIntent.setData(Uri.parse("package:" + getApplicationContext().getPackageName()));
+        mStartLauncher.launch(permissionIntent);
+    }
+}
---
+++ app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/NotificationController.java
@@ -0,0 +1,90 @@
+package jp.superwooo.chaipon.lighttimeswitcher;
+
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.example.minoru.myapplication.R;
+
+/**
+ * Created by Minoru on 2016/11/06.
+ */
+
+@RequiresApi(api = Build.VERSION_CODES.M)
+public class NotificationController {
+    private static final String CHANNEL_ID = "LightSwitcherNotification";
+    MainActivity mMainActivity;
+    public NotificationController(MainActivity mainActivity) {
+        mMainActivity = mainActivity;
+    }
+    private NotificationManager createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        NotificationManager notificationManager =
+                (NotificationManager)mMainActivity.getSystemService(mMainActivity.getApplicationContext().NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "LS";
+            String description = "LS";
+            int importance = NotificationManager.IMPORTANCE_LOW;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
+            channel.setDescription(description);
+            channel.setShowBadge(false);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            notificationManager.createNotificationChannel(channel);
+        }
+        return notificationManager;
+    }
+
+
+
+
+    public void notifyTimeOut() {
+        NotificationManager notificationManager = createNotificationChannel();
+        NotificationCompat.Builder notificationBuilder =
+                new NotificationCompat.Builder(mMainActivity, CHANNEL_ID);
+        notificationBuilder.setCategory(NotificationCompat.CATEGORY_MESSAGE);
+        setNotificationIcon(notificationBuilder);
+        setNotificationText(notificationBuilder);
+
+
+        setNotificationForever(notificationBuilder);
+
+        setApplicationToPushNotification(notificationBuilder);
+
+        Notification notification = notificationBuilder.build();
+
+        notificationManager.notify(1, notification);
+    }
+    private void setApplicationToPushNotification(NotificationCompat.Builder notificationBuilder) {
+        PendingIntent pending = PendingIntent.getActivity(mMainActivity,
+                0,
+                new Intent(mMainActivity, MainActivity.class),
+                PendingIntent.FLAG_IMMUTABLE);
+        notificationBuilder.setContentIntent(pending);
+    }
+    private void setNotificationForever(NotificationCompat.Builder notificationBuilder) {
+        notificationBuilder.setOngoing(true);
+    }
+
+    private void setNotificationText(NotificationCompat.Builder notificationBuilder) {
+        notificationBuilder.setContentTitle(mMainActivity.getString(R.string.lighting_time));
+        notificationBuilder.setContentText(mMainActivity.getTimeoutMessage());
+        notificationBuilder.setTicker(mMainActivity.getTimeoutMessage());
+    }
+
+    private void setNotificationIcon(NotificationCompat.Builder notificationBuilder) {
+        if(mMainActivity.isMinimumTimeOut()){
+            notificationBuilder.setSmallIcon(R.drawable.ic_stat_light_time_short);
+        }else{
+            notificationBuilder.setSmallIcon(R.drawable.ic_stat_light_time_long);
+        }
+    }
+}
