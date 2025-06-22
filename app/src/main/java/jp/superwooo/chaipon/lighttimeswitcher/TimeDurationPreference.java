9ee2f80bc8470dec2e82dc9e3b3be83785f28ad4
Minoru <mekun@be.to>
2025-06-22 14:02:35 +0900

    TimeDuration の Preferenceへの保存処理を TimeDurationPreferenceへ移動

--- app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/TimeDurationPreference.java
+++ app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/TimeDurationPreference.java
@@ -1,18 +1,24 @@
 package jp.superwooo.chaipon.lighttimeswitcher;
 
+import static android.widget.Toast.LENGTH_SHORT;
+import static android.widget.Toast.makeText;
+
 import android.content.SharedPreferences;
 import android.content.Context;
+import android.util.Log;
 
 import androidx.preference.PreferenceManager;
 
 public class TimeDurationPreference {
     private Context mContext;
     private ShortLongTimes mShortLongTimes;
+    private static final String MinimumKey = "minimumTime";
+    private static final String MaximumKey = "maximumTime";
     public TimeDurationPreference(Context context){
         mContext = context;
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext.getApplicationContext());
-        int shortSec = preferences.getInt(SettingsActivity.MinimumKey, MainActivity.MinTime / 1000);
-        int longSec = preferences.getInt(SettingsActivity.MaximumKey, MainActivity.MaxTime / 1000);
+        int shortSec = preferences.getInt(MinimumKey, MainActivity.MinTime / 1000);
+        int longSec = preferences.getInt(MaximumKey, MainActivity.MaxTime / 1000);
         mShortLongTimes = new ShortLongTimes(shortSec, longSec, SettingsActivity.LimitTime);
     }
     public ShortLongTimes getShortLongTimes() {
@@ -33,6 +39,20 @@ public class TimeDurationPreference {
             return DurationType.Long;
         else
             return DurationType.Short;
+    }
+    public void save(ShortLongTimes shortLongTimes)
+    {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(mContext.getApplicationContext());
+        SharedPreferences.Editor e = preferences.edit();
+        int minimum = shortLongTimes.getShortDuration().sec();
+        int maximum = shortLongTimes.getLongDuration().sec();
+        e.putInt(MinimumKey, minimum);
+        e.putInt(MaximumKey, maximum);
+        e.apply();
+        Log.d("LS", "save minimum: " + minimum);
+        Log.d("LS", "save maximum: " + maximum);
+        String message = mContext.getString(R.string.set_mini_max, minimum, maximum);
+        makeText(mContext, message, LENGTH_SHORT).show();
     }
 
 }
