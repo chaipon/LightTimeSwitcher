014f1e566d670c91a4391b520304314574ab35f7
Minoru <mekun@be.to>
2025-07-02 19:51:42 +0900

    正確なアラームに対応
    * permisionをManifestに定義
    * permision 設定画面へ誘導、起動処理を追加
    * set() -> setExact() へ変更
    * リソース追加

--- app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/AlarmScheduler.java
+++ app/src/main/java/jp/superwooo/chaipon/lighttimeswitcher/AlarmScheduler.java
@@ -4,6 +4,7 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.util.Log;
 
 import java.time.Duration;
@@ -11,30 +12,28 @@ import java.time.LocalTime;
 import java.util.Calendar;
 
 public class AlarmScheduler {
-    static String DurationTypeKey = "duration_type";
+    static final String DurationTypeKey = "duration_type";
     public static void scheduleTimeout(Context context, DurationType type, LocalTime scheduleTime) {
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         Log.d("LS", "schedule to" + scheduleTime.toString() + ". type: " + type.name());
-        alarmManager.setRepeating(
-                AlarmManager.RTC_WAKEUP,
-                getRtcTime(scheduleTime),
-                Duration.ofHours(24).toMillis(),
+        alarmManager.setExact(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                getElapsedTriggerTime(scheduleTime),
                 buildPendingIntent(context, type)
         );
     }
-    private static long getRtcTime(LocalTime scheduleTime){
+    private static long getElapsedTriggerTime(LocalTime scheduleTime){
         LocalTime now = LocalTime.now();
         long delayTime = Duration.between(now, scheduleTime).toMillis();
         if(delayTime < 0) {
-            Log.i("LS", "Invalid delay time: " + delayTime + ", type: " + DurationType.Long.name());
+            Log.i("LS", "Invalid delay time: " + delayTime);
             delayTime += Duration.ofHours(24).toMillis();
         }
         Log.d("LS", "After: " + delayTime + "ms");
-        Calendar cal = Calendar.getInstance();
-        cal.setTimeInMillis(System.currentTimeMillis() + delayTime);
-        return cal.getTimeInMillis();
+        return SystemClock.elapsedRealtime() + delayTime;
     }
     public static void cancel(Context context, DurationType type) {
+        Log.d("LS", "cancel: " + type.name());
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(buildPendingIntent(context, type));
     }
@@ -48,6 +47,18 @@ public class AlarmScheduler {
 
         if(enableShortTimePreference.isEnabled())
             scheduleTimeout(context, DurationType.Short, enableShortTimePreference.loadTime());
+
+    }
+    public static void cancelAll(Context context){
+        EnableTimePreference enableShortTimePreference = EnableTimePreference.create(context, SettingsActivity.EnableTimeShortKeyPref);
+        EnableTimePreference enableLongTimePreference = EnableTimePreference.create(context, SettingsActivity.EnableTimeLongKeyPref);
+        Log.d("LS", "cancel all schedule");
+
+        if(enableLongTimePreference.isEnabled())
+            cancel(context, DurationType.Long);
+
+        if(enableShortTimePreference.isEnabled())
+            cancel(context, DurationType.Short);
 
     }
     public static PendingIntent buildPendingIntent(Context context, DurationType type){
